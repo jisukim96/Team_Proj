6ce702f29c55c991aba5c888aafb58b503508b2d
Jisu Kim <yyjisu@gmail.com>
2023-03-23 10:14:55 +0900

    init 2023.03.23 - kjs

---
+++ src/main/java/com/Team_Proj01/TeamProj01Application.java
@@ -0,0 +1,13 @@
+package com.Team_Proj01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProj01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Team_Proj01/TeamProj01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Team_Proj01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProj01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
